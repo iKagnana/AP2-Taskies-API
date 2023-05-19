834361b75202ddc245a52d66a016f2438972ee9e
Kazeynma <kagnana.ith@ecole-isitech.fr>
2023-05-19 17:00:12 +0200

    resolve issues with token

--- src/config/token.ts
+++ src/config/token.ts
@@ -1,18 +1,11 @@
 import jwt from "jsonwebtoken";
 import * as dotenv from "dotenv";
 
+dotenv.config();
+
 const key = process.env.KEY || "";
 
 export const getToken = (payload: object) => {
-    console.log(payload)
-    jwt.sign(payload, key, {
-        expiresIn: "1d"
-    }, (err, token) => {
-        if (!err) {
-            console.log("here", token)
-            return token
-        } else {
-            console.log(err)
-        }
-    })
+    const accessToken = jwt.sign(payload, key, { expiresIn: "1d" })
+    return accessToken
 }
